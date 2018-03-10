e7d96dfcde5027ceb4a03ddd711ae712525ba79e
tamoorkayani <tamoorkayani@gmail.com>
2018-03-10 09:04:27 +0500

    Update on Boarding Pass App
    Update on Boarding Pass App

--- Contents/BoardingPass/js/scanner.js
+++ Contents/BoardingPass/js/scanner.js
@@ -230,13 +230,13 @@ console.log(parts_p);
                       }   
 
                       else{
-                       alert("Please put the Barcode scanner on the right position and try again.        يرجى وضع الماسح الضوئي الباركود على المكان الصحيح وحاول مرة أخرى.");
+                      /* alert("Please put the Barcode scanner on the right position and try again.        يرجى وضع الماسح الضوئي الباركود على المكان الصحيح وحاول مرة أخرى.");*/
                         location.reload();
                       }                     
               });
                  
                     setTimeout(function() {
                       $('#submit').trigger('click');
-                      }, 5000);
+                      }, 20000);
                  
         });
