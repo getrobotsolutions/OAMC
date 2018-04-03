aa9251fc9940a9069a7de84e3756ea18599553aa
tamoorkayani <tamoorkayani@gmail.com>
2018-04-04 00:24:14 +0500

    Header alignment fixed
    Header alignment fixed

--- Scripts/lang.js
+++ Scripts/lang.js
@@ -156,7 +156,7 @@ $(document).ready(function(){
         var image =  firstResult.code;
         var loc = 'https://s.yimg.com/zz/combo?a/i/us/we/52/'+image+'.gif' ;
   
-        $('#temp').append('The temperature is <strong>' + temp + '</strong><sup>°C</sup> Forecast calls for '+text);
+        $('#temp').append('The temperature is <strong>' + temp +'</strong><sup>°C</sup> Forecast calls for '+text);
         //$('#temp').append('<img style="width:70px;margin-right:20px;"         src="'+ loc +'"> <strong> ' + temp + '</strong><sup>°C</sup> ');
 
         //$('#image-zoom').attr("src",loc);
